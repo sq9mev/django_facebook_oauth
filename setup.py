0b9864f4cc05f990dbeb4134acafd087870e602c
Dominik Socek Długajczyk <dominik.dlugajczyk@codepill.com>
2012-01-31 10:58:43 +0100

    New version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-facebook-oauth',
-    version='2.0custom',
+    version='2.0.1custom',
     description="Facebook OAuth2 authentication for Django.",
     long_description=open('README.markdown').read(),
     author='Jeff Dickey',
