615d520a80e19463d1874fe265a41d3192eb40a4
Marcin Nowak <marcin.nowak@codepill.com>
2012-02-17 12:09:05 +0100

    setup version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-facebook-oauth',
-    version='2.0',
+    version='3.0-dev',
     description="Facebook OAuth2 authentication for Django.",
     long_description=open('README.markdown').read(),
     author='Jeff Dickey',
