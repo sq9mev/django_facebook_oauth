f0758e2ba07fb066672e6684b1ffd228ea86b50a
Marcin Nowak <marcin.nowak@codepill.com>
2012-02-20 09:09:46 +0100

    added south migrations

--- facebook/models.py
+++ facebook/models.py
@@ -1,16 +1,8 @@
 from django.db import models
 
 
-class AbstractFacebookProfile(models.Model):
+class FacebookProfile(models.Model):
     user = models.OneToOneField('auth.User')
     facebook_id = models.BigIntegerField()
     access_token = models.CharField(max_length=150, null=True, blank=True)
-
-    class Meta:
-        abstract = True
-
-
-class FacebookProfile(AbstractFacebookProfile):
-    pass
-
 
