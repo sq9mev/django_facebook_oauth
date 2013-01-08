4fc45f1c92490bfac5dcb16085a3a70e679ec267
Bartek Radwan <bartosz.radwan@proidea.org.pl>
2013-01-08 23:47:02 +0100

    FBConsoleBackenf->FBLogBackend - due to better tasks debugging

--- facebook/fbbackends.py
+++ facebook/fbbackends.py
@@ -3,6 +3,8 @@ from fbgraph import GraphAPI, GraphAPIError
 from tasks import *
 import logging
 
+logger=logging.getLogger('facebook')
+
 class FBGraphBackend(object):
     def post_pages(self, message, attachment=None):
             '''
@@ -11,7 +13,7 @@ class FBGraphBackend(object):
 
             FACEBOOK_OBJECTS_IDS=getattr(settings, 'FACEBOOK_OBJECTS_IDS')
             if not FACEBOOK_OBJECTS_IDS:
-                logging.debug('Not posting any FB pages - not configured')
+                logger.debug('Not posting any FB pages - not configured')
                 return
             attachment=attachment or {}
             for (fbobj_id, access_token) in FACEBOOK_OBJECTS_IDS:
@@ -19,7 +21,7 @@ class FBGraphBackend(object):
                 try:
                     graph.put_wall_post(message, profile_id=str(fbobj_id), attachment=attachment)
                 except GraphAPIError, e:
-                    logging.error('Problem posting FB: %s' % e)
+                    logger.error('Problem posting FB: %s' % e)
 
     def post_as_app(self, profile_id, message, attachment=None):
         attachment=attachment or {}
@@ -28,7 +30,7 @@ class FBGraphBackend(object):
         try:
             graph.put_wall_post(message, profile_id=str(profile_id), attachment=attachment)
         except GraphAPIError, e:
-            logging.error('Problem posting FB: %s' % e)
+            logger.error('Problem posting FB: %s' % e)
 
 
 class AsyncFBGraphBackend(FBGraphBackend):
@@ -40,17 +42,12 @@ class AsyncFBGraphBackend(FBGraphBackend):
         post_as_app.delay(profile_id, message, attachment)
 
 
-class FBConsoleBackend(object):
+class FBLogBackend(object):
 
     def post_pages(self, message, attachment=None):
-        print '-'*79
-        print 'Message: %s', message
-        print 'Attachement: %s', attachment
-        print '-'*79
+        logger.info( u'Message: %s\nAttachment: %s' %
+                (message, attachment))
 
     def post_as_app(self, profile_id, message, attachment=None):
-        print '-'*79
-        print 'Profile: %s' % profile_id
-        print 'Message: %s', message
-        print 'Attachement: %s', attachment
-        print '-'*79
+        logger.info(u'Profile: %s\nMessage: %s\nAttachment: %s' % 
+                (profile_id, message, attachment))
